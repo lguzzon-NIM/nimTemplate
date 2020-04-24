bfddc19a28f8107f1449ba6c55b094b5412b6e17
Luca Guzzon <luca.guzzon@gmail.com>
2020-04-24 17:40:49 +0000

    Wine fixups

--- scripts/linux/travis.sh
+++ scripts/linux/travis.sh
@@ -187,7 +187,7 @@ if [[ ${NIM_TARGET_OS} == "windows" ]]; then
   ${aptGetCmd} update
 
   installIfNotPresent mingw-w64
-  installIfNotPresent wine
+  installIfNotPresent wine-devel
   if [[ ${NIM_TARGET_CPU} == "i386" ]]; then
     echo "------------------------------------------------------------ targetCPU: ${NIM_TARGET_CPU}"
     export WINEARCH=win32
