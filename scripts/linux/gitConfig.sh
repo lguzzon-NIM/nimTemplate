9d35212e598367b58ebf81bc9ee018f3e83dba49
lguzzon <luca.guzzon@gmail.com>
2020-11-13 17:42:35 +0000

    chore(gitpod): nim config and git config

--- scripts/linux/gitConfig.sh
+++ scripts/linux/gitConfig.sh
@@ -30,11 +30,21 @@ git config --global --replace-all alias.sti "status --ignored"
 git config --global --replace-all alias.br "branch"
 
 # https://switowski.com/git/2019/01/18/7-git-functions-to-make-your-life-easier.html
-git config --global --replace-all alias.aliases "config --get-regexp alias"
+git config --global --replace-all alias.aliases "!git config --get-regexp alias | sort"
 git config --global --replace-all alias.squash '!f(){ git reset --soft HEAD~${1} && git commit --edit -m"$(git log --format=%B --reverse HEAD..HEAD@{1})"; }; f'
 
 # https://github.com/durdn/cfg/blob/master/.gitconfig
 git config --global --replace-all alias.sqc '!f(){ git reset --soft HEAD~$1 && git commit --edit -m"$(git log --format=%B --reverse HEAD..HEAD@{1})"; }; f'
+
+# https://www.freecodecamp.org/news/how-to-use-git-aliases/
+git config --global --replace-all alias.graph "log --oneline --graph --decorate"
+git config --global --replace-all alias.ls "log --pretty=format:'%C(yellow)%h%Cred%d - %Creset%s%Cblue - [%cn]' --decorate"
+git config --global --replace-all alias.ll "log --pretty=format:'%C(yellow)%h%Cred%d - %Creset%s%Cblue - [%cn]' --decorate --numstat"
+git config --global --replace-all alias.lds "log --pretty=format:'%C(yellow)%h - %ad%Cred%d - %Creset%s%Cblue - [%cn]' --decorate --date=short"
+git config --global --replace-all alias.conflicts "diff --name-only --diff-filter=U"
+git config --global --replace-all alias.brl "!git branch -vv | cut -c 3- | awk '\$3 !~/\\[/ { print \$1 }'"
+git config --global --replace-all alias.brr "!git branch --sort=-committerdate | head"
+git config --global --replace-all alias.authors "!git log --format='%aN <%aE>' | grep -v 'users.noreply.github.com' | sort -u --ignore-case"
 
 # https://git-scm.com/docs/git-credential-cache
 # Setting to half an hour
