eb2a83797da19bee9bd5ec675b3e17f0a36c2f5e
lguzzon <luca.guzzon@gmail.com>
2020-12-19 11:48:30 +0000

    chore(scripts): fix-ups

--- scripts/linux/installNim.sh
+++ scripts/linux/installNim.sh
@@ -1,7 +1,46 @@
 #!/bin/bash
 
-(hash curl 2>/dev/null || sudo apt -y install curl) &&
-    curl https://nim-lang.org/choosenim/init.sh -sSf | CHOOSE_VERSION=devel sh -s -- -y &&
-    export PATH="$HOME/.nimble/bin${PATH:+:$PATH}" &&
-    echo "export PATH=\"\$HOME/.nimble/bin\${PATH:+:\$PATH}\"" >>"${HOME}/.bashrc" &&
-    nim --version
+export APPS_DIR_NAME=APPs
+export APPS_PATH="${HOME}/${APPS_DIR_NAME}"
+[ -d "/data" ] && APPS_PATH="/data/${APPS_DIR_NAME}"
+echo "APPS_PATH [${APPS_PATH}]"
+mkdir -p "${APPS_PATH}"
+
+readonly lLinuxArchitecture=$(uname -m)
+echo "Linux Architecture: ${lLinuxArchitecture}"
+lArchitecture=${lLinuxArchitecture}
+case ${lLinuxArchitecture} in
+  aarch64*)
+    lArchitecture="arm64"
+    ;;
+  x86_64*)
+    lArchitecture="x64"
+    ;;
+esac
+echo "Tool  Architecture: ${lArchitecture}"
+
+TOOL_NAME="NIM"
+TOOL_URL="$(curl -sSL https://api.github.com/repos/nim-lang/nightlies/releases | jq -r "[ .[]?.assets[] | select(.browser_download_url | test(\"latest-devel/linux_$lArchitecture\")) | {updated_at, browser_download_url} ] | sort_by(.updated_at) | reverse | .[0].browser_download_url")"
+case $1 in
+  latestVersion*)
+    TOOL_URL="$(curl -sSL https://api.github.com/repos/nim-lang/nightlies/releases | jq -r "[ .[]?.assets[] | select(.browser_download_url | test(\"latest-version-\")) | select(.browser_download_url | test(\"linux_$lArchitecture\")) | {updated_at, browser_download_url} ] | sort_by(.browser_download_url) | reverse | .[0].browser_download_url")"
+    ;;
+esac
+
+(hash curl 2>/dev/null || sudo apt -y install curl) \
+  && (hash jq 2>/dev/null || sudo apt -y install jq) \
+  && curl -o nim.tar.xz -sSL "${TOOL_URL}" \
+  && (rm -rf "$(dirname "$(dirname "$(which nim)")")" 2>/dev/null \
+    || rm -rf "$APPS_PATH/nim" 2>/dev/null \
+    || true) \
+  && tar -xvf nim.tar.xz \
+  && rm nim.tar.xz || true \
+  && mv nim-* "$APPS_PATH/nim" \
+  && export PATH="$APPS_PATH/nim/bin${PATH:+:$PATH}" \
+  && sed "/### +++ ${TOOL_NAME} +++ ###/,/### --- ${TOOL_NAME} --- ###/d" -i "${HOME}/.bashrc" \
+  && {
+    echo "### +++ ${TOOL_NAME} +++ ###"
+    echo "[ -d \"$APPS_PATH/nim/bin\" ] && export PATH=\"$APPS_PATH/nim/bin\${PATH:+:\$PATH}\""
+    echo "### --- ${TOOL_NAME} --- ###"
+  } >>"${HOME}/.bashrc" \
+  && nim --version
--- scripts/linux/installZig.sh
+++ scripts/linux/installZig.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 
-(hash curl || sudo apt -y install curl) &&
-    (hash jq || sudo apt -y install jq) &&
-    curl -o zig.tar.xz -sSL "$(curl -slL "https://ziglang.org/download/index.json" |
-        jq -r ".master[\"$(uname -m)-linux\"].tarball")" &&
-    tar -xvf zig.tar.xz &&
-    rm zig.tar.xz || true &&
-    mv zig-linux-$(uname -m)* "$HOME/.zig" &&
-    export PATH="$HOME/.zig${PATH:+:$PATH}" &&
-    echo "export PATH=\"\$HOME/.zig\${PATH:+:\$PATH}\"" >>"${HOME}/.bashrc" &&
-    zig version
+export APPS_DIR_NAME=APPs
+export APPS_PATH="${HOME}/${APPS_DIR_NAME}"
+[ -d "/data" ] && APPS_PATH="/data/${APPS_DIR_NAME}"
+echo "APPS_PATH [${APPS_PATH}]"
+mkdir -p "${APPS_PATH}"
+
+readonly lLinuxArchitecture=$(uname -m)
+echo "Linux Architecture: ${lLinuxArchitecture}"
+lArchitecture=${lLinuxArchitecture}
+# case ${lLinuxArchitecture} in
+#   aarch64*)
+#     lArchitecture="arm64"
+#     ;;
+#   x86_64*)
+#     lArchitecture="x64"
+#     ;;
+# esac
+echo "Tool  Architecture: ${lArchitecture}"
+
+TOOL_NAME="ZIG"
+
+(hash curl || sudo apt -y install curl) \
+  && (hash jq || sudo apt -y install jq) \
+  && curl -o zig.tar.xz -sSL "$(curl -slL "https://ziglang.org/download/index.json" \
+    | jq -r ".master[\"${lArchitecture}-linux\"].tarball")" \
+  && (rm -rf "$(dirname "$(which zig)")" 2>/dev/null \
+    || rm -rf "$APPS_PATH/zig" 2>/dev/null \
+    || true) \
+  && tar -xvf zig.tar.xz \
+  && rm zig.tar.xz || true \
+  && mv zig-linux-"${lArchitecture}"* "$APPS_PATH/zig" \
+  && export PATH="$APPS_PATH/zig${PATH:+:$PATH}" \
+    && sed "/### +++ ${TOOL_NAME} +++ ###/,/### --- ${TOOL_NAME} --- ###/d" -i "${HOME}/.bashrc" \
+  && {
+    echo "### +++ ${TOOL_NAME} +++ ###"
+    echo "[ -d \"$APPS_PATH/zig\" ] && export PATH=\"$APPS_PATH/zig\${PATH:+:\$PATH}\""
+    echo "### --- ${TOOL_NAME} --- ###"
+  } >>"${HOME}/.bashrc" \
+&& zig version
