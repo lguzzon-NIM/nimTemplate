cd1cc20ee80877816eae13effa953aa769c339fd
Luca Guzzon <luca.guzzon@gmail.com>
2021-08-15 11:58:33 +0200

    chore(scripts): fix-ups

--- scripts/linux/installV.sh
+++ scripts/linux/installV.sh
@@ -6,14 +6,15 @@ export APPS_PATH="${HOME}/${APPS_DIR_NAME}"
 echo "APPS_PATH [${APPS_PATH}]"
 mkdir -p "${APPS_PATH}"
 
-(hash git || sudo apt -y install git) \
-  && pushd "${APPS_PATH}" \
-  && ( 
-    ([ -d v ] || git clone "https://github.com/vlang/v") \
-      && cd v \
-      && make \
-      && sudo ./v symlink \
-      && v doctor \
+(hash make &>/dev/null || sudo apt -y install build-essential || sudo apt update && sudo apt -y install build-essential) &&
+  (hash git || sudo apt -y install git) &&
+  pushd "${APPS_PATH}" &&
+  (
+    ([ -d v ] || git clone "https://github.com/vlang/v") &&
+      cd v &&
+      make &&
+      sudo ./v symlink &&
+      v doctor \
       ;
     popd || exit 1
   )
